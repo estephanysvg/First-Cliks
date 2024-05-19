2a2153dae0345556c82d99f08eed81c18b889f16
estephsvg <166448101+estephsvg@users.noreply.github.com>
2024-05-19 17:52:25 +0200

    Carrousel

--- src/app/pages/home/course/course.component.ts
+++ src/app/pages/home/course/course.component.ts
@@ -1,14 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CarouselModule } from 'primeng/carousel';
 
+interface Course {
+  id: string;
+  name: string;
+  description: string;
+  image: string;
+}
 
 @Component({
   selector: 'app-course',
   standalone: true,
-  imports: [],
+  imports: [CarouselModule],
   templateUrl: './course.component.html',
-  styleUrl: './course.component.css'
+  styleUrls: ['./course.component.css']
 })
-export default class CourseComponent {
-  
+export default class CourseComponent implements OnInit {
+  courses: Course[] = [];
+  responsiveOptions: any[] = [];
 
+  ngOnInit() {
+    this.courses = [
+      {
+        id: '1',
+        name: 'Curso de lógica e programación',
+        description: 'Lorem imoiwe wefwe werwer werwer werwer wer',
+        image: 'course1.jpg'
+      },
+      {
+        id: '2',
+        name: 'Curso de HTML',
+        description: 'Lorem imoiwe wefwe werwer werwer werwer wer',
+        image: 'course2.jpg'
+      },
+      {
+        id: '3',
+        name: 'Curso de CSS',
+        description: 'Lorem imoiwe wefwe werwer werwer werwer wer',
+        image: 'course3.jpg'
+      },
+      {
+        id: '4',
+        name: 'Curso de Cobol',
+        description: 'Lorem imoiwe wefwe werwer werwer werwer wer',
+        image: 'course3.jpg'
+      }
+      // Añade más cursos según sea necesario
+    ];
+
+    this.responsiveOptions = [
+      {
+        breakpoint: '1024px',
+        numVisible: 3,
+        numScroll: 3
+      },
+      {
+        breakpoint: '768px',
+        numVisible: 2,
+        numScroll: 2
+      },
+      {
+        breakpoint: '560px',
+        numVisible: 1,
+        numScroll: 1
+      }
+    ];
+  }
 }
